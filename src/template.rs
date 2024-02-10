8373290a737031159fab3334640afa4ea0512cdd
STNeto1 <gfreitasneto18@gmail.com>
2024-02-10 20:19:19 -0300

    refactor: split generation

--- src/template.rs
+++ src/template.rs
@@ -6,32 +6,100 @@ use crate::sanitizer;
 
 pub fn generate_file_lines(schema: parser::Schema) -> Vec<String> {
     let mut lines: Vec<String> = Vec::new();
-    let mut result: HashMap<String, String> = HashMap::new();
 
-    schema.definitions.iter().for_each(|(key, value)| {
-        let parsed_key = parser::normalize_key(key);
+    generate_baselines(&mut lines);
+    generate_definition_types(&schema, &mut lines);
 
-        let raw_type = if value._enum.is_some() {
-            parser::parse_enum(value)
+    schema.paths.iter().for_each(|(key, value)| {
+        if value.get.is_none() {
+            warn!("No get method for {}", key);
+            return;
+        }
+
+        let _get = value.get.clone().unwrap();
+
+        let _2xx_responses = _get
+            .responses
+            .iter()
+            .filter(|(key, _)| key.starts_with("2"))
+            .collect::<Vec<_>>();
+        let non_2xx_responses = _get
+            .responses
+            .iter()
+            .filter(|(key, _)| key.starts_with("4") || key.starts_with("5"))
+            .collect::<Vec<_>>();
+
+        let query_type = if value.get.is_some() {
+            _get.parse_query()
         } else {
-            parser::create_raw_type_from_properties(&value.properties.clone().unwrap_or_default())
+            unimplemented!("No query type for {}", key)
         };
 
-        result.insert(parsed_key, raw_type);
-    });
+        let path_type = if value.get.is_some() {
+            _get.parse_path()
+        } else {
+            unimplemented!("No path type for {}", key)
+        };
 
-    // sort the keys from the hashmap
-    let mut keys: Vec<&String> = result.keys().collect();
-    keys.sort();
+        let tmp_key = sanitizer::create_input_type_name_from_path(key);
+        let fn_name = generate_fn_name("get".to_string(), key.to_string());
 
-    keys.iter().for_each(|key| {
         lines.push(format!(
-            "export type {} = {};\n",
-            key,
-            result.get(*key).unwrap()
+            "\n\ntype {tmp_key} = {{ query: {{{query_type}}}, path: {{{path_type}}} }};\n"
+        ));
+
+        lines.push(format!(
+            "type {fn_name}_response = {};\n",
+            _2xx_responses
+                .iter()
+                .map(|(_, value)| value.parse_response())
+                .collect::<Vec<_>>()
+                .join(" | ")
+        ));
+        lines.push(format!(
+            "type {fn_name}_error = {};\n",
+            if non_2xx_responses.is_empty() {
+                "never".to_string()
+            } else {
+                non_2xx_responses
+                    .iter()
+                    .map(|(_, value)| value.parse_response())
+                    .collect::<Vec<_>>()
+                    .join(" | ")
+            }
+        ));
+        lines.push(format!(
+            r#"export async function get_{fn_name}(props: {tmp_key}) {{
+    return fetcher<{fn_name}_response, {fn_name}_error>("{key}", props);
+}}
+"#
         ));
     });
 
+    return lines;
+}
+
+fn generate_fn_name(method: String, path: String) -> String {
+    let mut fn_name = method.to_lowercase();
+    fn_name.push_str("_");
+
+    path.split("/").filter(|&x| !x.is_empty()).for_each(|x| {
+        if x.starts_with("{") && x.ends_with("}") {
+            fn_name.push_str("by_");
+        } else {
+            fn_name.push_str(x);
+            fn_name.push_str("_");
+        }
+    });
+
+    if fn_name.ends_with("_") {
+        fn_name = fn_name.trim_end_matches('_').to_string();
+    }
+
+    fn_name
+}
+
+fn generate_baselines(lines: &mut Vec<String>) {
     lines.push(format!(
         r#"
 type Record = {{ [key: string]: string | number | boolean | undefined }};
@@ -108,92 +176,32 @@ async function mutator<TBody, TResult, TErr>(
 
 "#
     ));
-
-    schema.paths.iter().for_each(|(key, value)| {
-        if value.get.is_none() {
-            warn!("No get method for {}", key);
-            return;
-        }
-
-        let _get = value.get.clone().unwrap();
-
-        let _2xx_responses = _get
-            .responses
-            .iter()
-            .filter(|(key, _)| key.starts_with("2"))
-            .collect::<Vec<_>>();
-        let non_2xx_responses = _get
-            .responses
-            .iter()
-            .filter(|(key, _)| key.starts_with("4"))
-            .collect::<Vec<_>>();
-
-        let query_type = if value.get.is_some() {
-            _get.parse_query()
-        } else {
-            unimplemented!("No query type for {}", key)
-        };
-
-        let path_type = if value.get.is_some() {
-            _get.parse_path()
-        } else {
-            unimplemented!("No path type for {}", key)
-        };
-
-        let tmp_key = sanitizer::create_input_type_name_from_path(key);
-        let fn_name = generate_fn_name("get".to_string(), key.to_string());
-
-        lines.push(format!(
-            "\n\ntype {tmp_key} = {{ query: {{{query_type}}}, path: {{{path_type}}} }};\n"
-        ));
-
-        lines.push(format!(
-            "type {fn_name}_response = {};\n",
-            _2xx_responses
-                .iter()
-                .map(|(_, value)| value.parse_response())
-                .collect::<Vec<_>>()
-                .join(" | ")
-        ));
-        lines.push(format!(
-            "type {fn_name}_error = {};\n",
-            if non_2xx_responses.is_empty() {
-                "never".to_string()
-            } else {
-                non_2xx_responses
-                    .iter()
-                    .map(|(_, value)| value.parse_response())
-                    .collect::<Vec<_>>()
-                    .join(" | ")
-            }
-        ));
-        lines.push(format!(
-            r#"export async function get_{fn_name}(props: {tmp_key}) {{
-    return fetcher<{fn_name}_response, {fn_name}_error>("{key}", props);
-}}
-"#
-        ));
-    });
-
-    return lines;
 }
 
-fn generate_fn_name(method: String, path: String) -> String {
-    let mut fn_name = method.to_lowercase();
-    fn_name.push_str("_");
+fn generate_definition_types(schema: &parser::Schema, lines: &mut Vec<String>) {
+    let mut result: HashMap<String, String> = HashMap::new();
 
-    path.split("/").filter(|&x| !x.is_empty()).for_each(|x| {
-        if x.starts_with("{") && x.ends_with("}") {
-            fn_name.push_str("by_");
+    schema.definitions.iter().for_each(|(key, value)| {
+        let parsed_key = parser::normalize_key(key);
+
+        let raw_type = if value._enum.is_some() {
+            parser::parse_enum(value)
         } else {
-            fn_name.push_str(x);
-            fn_name.push_str("_");
-        }
+            parser::create_raw_type_from_properties(&value.properties.clone().unwrap_or_default())
+        };
+
+        result.insert(parsed_key, raw_type);
     });
 
-    if fn_name.ends_with("_") {
-        fn_name = fn_name.trim_end_matches('_').to_string();
-    }
+    // sort the keys from the hashmap
+    let mut keys: Vec<&String> = result.keys().collect();
+    keys.sort();
 
-    fn_name
+    keys.iter().for_each(|key| {
+        lines.push(format!(
+            "export type {} = {};\n",
+            key,
+            result.get(*key).unwrap()
+        ));
+    });
 }
