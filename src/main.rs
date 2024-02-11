f8b8356c100880be09be0627042fe35b941e3cee
STNeto1 <gfreitasneto18@gmail.com>
2024-02-11 08:43:25 -0300

    feat: create path to file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use std::fs::{self, File};
+use anyhow::{Ok, Result};
+use std::fs::{self, create_dir_all, File};
 use std::io::prelude::*;
 
 mod parser;
 mod sanitizer;
 mod template;
 
-fn main() {
+fn main() -> Result<()> {
     let input_file = fs::read_to_string("input.json").expect("Unable to read file");
-    let mut output_file = File::create("types.ts").expect("Unable to create file");
+
+    let _path = "lib/types.ts";
+    let folder_path = _path
+        .split("/")
+        .filter(|f| !f.ends_with(".ts"))
+        .collect::<Vec<_>>()
+        .join("/");
+
+    create_dir_all(folder_path)?;
+    let mut output_file = File::create("lib/types.ts").expect("Unable to create file");
 
     let schema: parser::Schema = serde_json::from_str(&input_file).expect("Unable to parse JSON");
 
@@ -18,4 +28,6 @@ fn main() {
                 .write_all(line.as_bytes())
                 .expect("Unable to write data");
         });
+
+    Ok(())
 }
